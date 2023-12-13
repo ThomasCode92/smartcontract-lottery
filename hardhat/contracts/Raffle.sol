f1b8358b6310dd7d13aeb0f99ee44f9d9f280696
ThomasCode92 <thomascode92@hotmail.com>
2023-12-13 20:08:25 +0100

    Raffle.sol Setup

---
+++ hardhat/contracts/Raffle.sol
@@ -0,0 +1,33 @@
+// Raffle
+// Enter the lottery (paying some amount)
+// Pick a random winner (verifiably random)
+// Winner to be selected every X minutes => completly automated
+// Chainlink Oracle => Randomness, Automated Execution (Chainlink Keeper)
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+error Raffle_NotEnoughETHEntered();
+
+contract Raffle {
+    /* State Variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) revert Raffle_NotEnoughETHEntered();
+        s_players.push(payable(msg.sender));
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 playerIdx) public view returns (address) {
+        return s_players[playerIdx];
+    }
+}
