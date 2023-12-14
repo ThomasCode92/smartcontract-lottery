922c88185dc5b3399eeb31c85880e735abfabd53
ThomasCode92 <thomascode92@hotmail.com>
2023-12-14 21:55:01 +0100

    Introduction to Chainlink VRF

--- hardhat/contracts/Raffle.sol
+++ hardhat/contracts/Raffle.sol
@@ -7,25 +7,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
+
 error Raffle_NotEnoughETHEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
+    /* Events */
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(
+        address vrfCoordinator,
+        uint256 entranceFee
+    ) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
     }
 
+    /* Functions */
     function enterRaffle() public payable {
         if (msg.value < i_entranceFee) revert Raffle_NotEnoughETHEntered();
         s_players.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
     }
 
+    function requestRandomWinner() external {
+        // Request the random number from the oracle
+        // Once we get it, do somethign with it
+        // 2 transaction process
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] memory randomWords
+    ) internal override {}
+
+    /* View/ Pure Functions */
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
